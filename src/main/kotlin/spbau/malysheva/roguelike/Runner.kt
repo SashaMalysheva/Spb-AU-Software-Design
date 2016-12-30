4c1dbdc79fef902d81710067fe33651162927a9b
SashaMalysheva <malyshevasasha777@gmail.com>
2016-12-30 21:37:36 +0300

    redraw on startup

--- src/main/kotlin/spbau/malysheva/roguelike/Runner.kt
+++ src/main/kotlin/spbau/malysheva/roguelike/Runner.kt
@@ -4,6 +4,7 @@ import spbau.malysheva.roguelike.controller.Environment
 
 fun main(args: Array<String>) {
     Environment.newGame()
+    Environment.view?.redraw()
 
     var command = readLine()
     while (command != null && command != ":quit") {
